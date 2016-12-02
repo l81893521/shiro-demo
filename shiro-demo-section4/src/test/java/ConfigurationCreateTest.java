a6281b8d7f1b95a191781008f7af8632eb36df97
l81893521 <349071347@qq.com>
2016-12-02 15:49:35 +0800

    update section4

---
+++ shiro-demo-section4/src/test/java/ConfigurationCreateTest.java
@@ -0,0 +1,27 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Will.Zhang on 2016/12/2 0002 15:46.
+ */
+public class ConfigurationCreateTest {
+
+    @Test
+    public void test(){
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-config.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        subject.login(token);
+
+        Assert.assertTrue(subject.isAuthenticated());
+    }
+}
