96c9e2061df8bd5705129eb86e90535ee4156e19
l81893521 <349071347@qq.com>
2016-12-05 16:24:33 +0800

    update section5

--- shiro-demo-section5/src/test/java/PasswordTest.java
+++ shiro-demo-section5/src/test/java/PasswordTest.java
@@ -1,3 +1,5 @@
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Test;
 
 /**
@@ -13,5 +15,26 @@ public class PasswordTest extends BaseTest{
     @Test
     public void testPasswordServiceWithJdbcRealm(){
         login("classpath:shiro-jdbc-passwordService.ini","zhang","123");
+    }
+
+    @Test
+    public void testGeneratePassword(){
+        String algorithmName = "md5";
+        String username = "liu";
+        String password = "123";
+        String salt1 = username;
+        String salt2 = new SecureRandomNumberGenerator().nextBytes().toHex();
+        int hashIteration = 2;
+
+        SimpleHash simpleHash = new SimpleHash(algorithmName, password, salt1 + salt2, hashIteration);
+        String encodePassword = simpleHash.toHex();
+        System.out.println(salt2);
+        System.out.println(encodePassword);
+    }
+
+    @Test
+    public void testHashedCredentialsMatcherWithMyRealm2(){
+        //使用testGeneratePassword生成的散列密码
+        login("classpath:shiro-hashedCredentialsMatcher.ini", "liu", "123");
     }
 }
