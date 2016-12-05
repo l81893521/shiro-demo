290ce0ec389c40907c3732eea43a4ccb122e4068
l81893521 <349071347@qq.com>
2016-12-05 16:38:37 +0800

    update section5

--- shiro-demo-section5/src/test/java/PasswordTest.java
+++ shiro-demo-section5/src/test/java/PasswordTest.java
@@ -1,5 +1,8 @@
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.converters.AbstractConverter;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.junit.Test;
 
 /**
@@ -36,5 +39,28 @@ public class PasswordTest extends BaseTest{
     public void testHashedCredentialsMatcherWithMyRealm2(){
         //使用testGeneratePassword生成的散列密码
         login("classpath:shiro-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+    @Test
+    public void testHashedCredentialsMatcherWithJdbcRealm(){
+
+        BeanUtilsBean.getInstance().getConvertUtils().register(new EnumConverter(), JdbcRealm.SaltStyle.class);
+        //使用testGeneratePassword生成的散列密码
+        login("classpath:shiro-jdbc-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+    private class EnumConverter extends AbstractConverter {
+
+        protected String convertToString(final Object value) throws Throwable {
+            return ((Enum)value).name();
+        }
+
+        protected Object convertToType(Class type, Object value) throws Throwable {
+            return Enum.valueOf(type, value.toString());
+        }
+
+        protected Class getDefaultType() {
+            return null;
+        }
     }
 }
